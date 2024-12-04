a7263e7359b125f1048f10ac61b5fe471b8d3ddc
Programador Full Stack 2024.2 <16133232024.2@senacrjedu.local>
2024-12-04 19:48:11 -0300

    começo da lista de imagem

--- js/teste.js
+++ js/teste.js
@@ -23,6 +23,8 @@
             } else {
                 resultsDiv.innerHTML = '<p>Please enter a game title.</p>';
             }
+
+            showImg();
         }
 
 
@@ -31,5 +33,11 @@ function showImg(){
     const img = document.createElement('img');
     img.src = "../img/cyberpunk.jpg";
     img.alt = "cyberpunk";
+
+    img.width = 200; // Set width in pixels
+    img.height = 200; // Set height in pixels
+
+    const container = document.getElementById('imageContainer');
+    container.appendChild(img);
 
 }
